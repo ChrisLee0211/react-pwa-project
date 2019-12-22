4176937ce84c179cec92c554a8d84aaa9b39f0ce
Chrislee0211 <1018596741@qq.com>
2019-12-22 22:41:11 +0800

    初始化目录

---
+++ src/index/App.jsx
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import './App.css';
+
+function App(props){
+
+}
+
+const mapStateToProps = (state) => {
+  
+}
+const mapDispatchToProps = (state) => {
+
+}
+
+export default connect(
+  mapStateToProps,mapDispatchToProps
+)(App)
