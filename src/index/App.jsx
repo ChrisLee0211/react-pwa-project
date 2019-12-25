4e3f7a08d2eedcca408a0cc4bd44eb4b9c6e0b61
Chrislee0211 <1018596741@qq.com>
2019-12-25 23:30:44 +0800

    搭建基本目录结构

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,15 +1,30 @@
 import { connect } from 'react-redux';
+import React from 'react';
 import './App.css';
 
-function App(props){
+import Header from '../common/Header';
+import DepartDate from './DepartDate';
+import HighSpeed from './HighSpeed';
+import Journey from './Journey';
+import Submit from './Submit'
 
+function App(props){
+  return(
+    <div>
+      <Header/>
+      <Journey/>
+      <DepartDate/>
+      <HighSpeed/>
+      <Submit/>
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
-  
+  return {}
 }
 const mapDispatchToProps = (state) => {
-
+  return{}
 }
 
 export default connect(
