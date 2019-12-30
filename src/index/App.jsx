325938a96bbd68db09a015ac4c949a7284e8edeb
Chrislee0211 <1018596741@qq.com>
2019-12-30 23:08:26 +0800

    增加点击城市浮层

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
-import React,{useCallback} from 'react';
+import React,{useCallback,useMemo} from 'react';
+import {bindActionCreators} from 'redux'
 import './App.css';
 
 import Header from '../common/Header';
@@ -7,33 +8,35 @@ import DepartDate from './DepartDate';
 import HighSpeed from './HighSpeed';
 import Journey from './Journey';
 import Submit from './Submit'
+import CitySelector from '../common/CitySelector';
 import {exchangeFromTo,showCitySelector} from './actions'
 
 function App(props){
-  const {from,to,dispatch} = props
+  const {from, isCitySelectorVisible, cityData , isLoadingCityData ,to,dispatch} = props
   const onBack = useCallback(() => {
     window.history.back();
   },[])
-  const doExchangeFromTo = useCallback(()=>{
-    dispatch(exchangeFromTo())
-  },[])
-  const doShowCitySelector = useCallback((m)=>{
-    dispatch(showCitySelector(m))
-  },[])
+  const cbs = useMemo(()=>{
+    return bindActionCreators({
+      exchangeFromTo,showCitySelector,
+    },dispatch)
+  })
   return(
     <div>
       <div className="header-wrapper">
         <Header title="火车票" onBack={onBack}/>
       </div>
-      <Journey 
-      from={from} 
-      to={to}
-      exchangeFromTo={doExchangeFromTo}
-      showCitySelector={doShowCitySelector}
-      />
-      <DepartDate/>
-      <HighSpeed/>
-      <Submit/>
+      <form className="form">
+        <Journey 
+        from={from} 
+        to={to}
+        {...cbs}
+        />
+        <DepartDate/>
+        <HighSpeed/>
+        <Submit/>
+      </form>
+      <CitySelector show={isCitySelectorVisible} cityData={cityData} isLoading={isLoadingCityData}/>
     </div>
   )
 }
