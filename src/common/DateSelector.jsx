03087b05a072ed3cd2d8c31f79c05cd78de1ff23
Chrislee0211 <1018596741@qq.com>
2020-01-19 18:03:04 +0800

    日期选择浮层增加年月周

--- src/common/DateSelector.jsx
+++ src/common/DateSelector.jsx
@@ -1,11 +1,69 @@
 import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
-import './DateSelector.css';
-
 import Header from './Header';
 
+import './DateSelector.css';
+import { tsNumberKeyword } from '@babel/types';
 
+function Month(props) {
+    const {
+        startTingTimeInMonth,
+        onSelect,
+    } = props;
+    const startDay = new Date(startTingTimeInMonth);
+    const currentDay = new Date(startTingTimeInMonth);
+
+    let days = [];
+
+    while(currentDay.getMonth()===startDay.getMonth()){
+        days.push(currentDay.getTime());
+        currentDay.setDate(currentDay.getDate()+1)
+    }
+
+    days = new Array(startDay.getDay()?startDay.getDay()-1:6)
+        .fill(null).concat(days);
+
+    const lastDay = new Date(days[days.length-1]);
+
+    days = days.concat(new Array(lastDay.getDay()?7 - lastDay.getDay():0).fill(null));
+
+    const weeks = [];
+    for(let row=0; row<days.length/7;++row){
+        const week = days.slice(row*7,(row+1)*7)
+        weeks.push(week)
+    }
+
+    return(
+        <table className="date-table">
+            <thead>
+                <tr>
+                    <td colSpan="7">
+                        <h5>
+                            {startDay.getFullYear()}年{startDay.getMonth()+1}月
+                        </h5>
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr className="data-table-weeks">
+                    <th>周一</th>
+                    <th>周二</th>
+                    <th>周三</th>
+                    <th>周四</th>
+                    <th>周五</th>
+                    <th className="weekend">周六</th>
+                    <th className="weekend">周日</th>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+Month.propTypes = {
+    startTingTimeInMonth:PropTypes.number.isRequired,
+    onSelect:PropTypes.func.isRequired
+}
 
 export default function DateSelector(props) {
     const {
@@ -14,13 +72,40 @@ export default function DateSelector(props) {
         onBack
     } = props
 
+    const now = new Date();
+    now.setHours(0);
+    now.setMinutes(0);
+    now.setSeconds(0);
+    now.setMilliseconds(0);
+    now.setDate(1);
+
+    const monthSequence = [now.getTime()];
+
+    now.setMonth(now.getMonth()+1);
+    monthSequence.push(now.getTime());
+
+    now.setMonth(now.getMonth()+1);
+    monthSequence.push(now.getTime());
+
+
     return (
         <div className={classnames('date-selector',{hidden:!show})}>
             <Header title="日期选择" onBack={onBack}/>
-            <div className="date-selector-tables"></div>
+            <div className="date-selector-tables">
+                {monthSequence.map(month => {
+                    return (
+                        <Month 
+                            key={month}
+                            onSelect={onSelect}
+                            startTingTimeInMonth={month}
+                        />
+                    )
+                })}
+            </div>
         </div>
     )
 }
+
 
 DateSelector.propTypes = {
     show:PropTypes.bool.isRequired,
