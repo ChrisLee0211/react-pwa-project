cd706e5ecedf492345991cc9336ae7a44efa102d
Chrislee0211 <1018596741@qq.com>
2020-02-05 23:02:23 +0800

    新增座次下拉选择

--- src/ticket/Candidate.jsx
+++ src/ticket/Candidate.jsx
@@ -1,35 +1,73 @@
-import React,{memo} from 'react';
+import React,{memo, useState, useCallback} from 'react';
 import PropTypes from 'prop-types';
 import './Candidate.css';
+
+const Channel = memo(function Channel(props){
+    const {
+        name,
+        desc,
+        type
+    } = props
+
+    return (
+        <div className="channel">
+            <div className="middle">
+                <div className="name">{name}</div>
+                <div className="desc">{desc}</div>
+            </div>
+            <a href="" className="buy-wrapper">
+                <div className="buy">买票</div>
+            </a>
+        </div>
+    )
+})
+
+Channel.propTypes = {
+    name:PropTypes.string.isRequired,
+    desc:PropTypes.string.isRequired,
+    type:PropTypes.string.isRequired,
+}
 
 const Seat = memo(function Seat(props){
     const {
         type,
         priceMsg,
         ticketsLeft,
-        channels
+        channels,
+        expanded,
+        onToggle,
+        idx
     } = props;
     return (
         <li>
-            <div className="bar">
+            <div className="bar" onClick={()=>onToggle(idx)}>
                 <span className="seat">{type}</span>
                 <span className="price">
                     <i>¥</i>
                     {priceMsg}
                 </span>
-                <span className="btn">收起</span>
+                <span className="btn">{expanded?'预定':'收起'}</span>
                 <span className="num">{ticketsLeft}</span>
             </div>
-            <div className="channels"></div>
+            <div className="channels" style={{height:expanded?channels.length *55 + 'px':0}}> 
+                {
+                    channels.map(channel => {
+                        return <Channel key={channel.name} {...channel} type={type}/>
+                    })
+                }
+            </div>
         </li>
     )
 })
 
 Seat.propTypes = {
     type:PropTypes.string.isRequired,
-priceMsg:PropTypes.string.isRequired,
-ticketsLeft:PropTypes.string.isRequired,
-channels:PropTypes.array.isRequired,
+    priceMsg:PropTypes.string.isRequired,
+    ticketsLeft:PropTypes.string.isRequired,
+    channels:PropTypes.array.isRequired,
+    expanded:PropTypes.bool.isRequired,
+    onToggle:PropTypes.func.isRequired,
+    idx:PropTypes.number.isRequired,
 }
 
 const Candidate = memo(
@@ -39,12 +77,17 @@ const Candidate = memo(
             tickets
         } = props
 
+        const [expandedIndex,setExpandedIndex] = useState(-1)
+        const onToggle = useCallback(idx => {
+            setExpandedIndex(idx === expandedIndex? -1:idx)
+        },[expandedIndex]);
+
         return (
             <div className="candidate">
                 <ul>
                     {
                         tickets.map((ticket,idx)=>{
-                            return <Seat {...ticket} key={ticket.type} />
+                            return <Seat idx={idx} onToggle={onToggle} expanded={expandedIndex===idx?true:false} {...ticket} key={ticket.type} />
                         })
                     }
                 </ul>
